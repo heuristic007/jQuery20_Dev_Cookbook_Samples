5bf238419281334291796b9c389f73b7b6db1c36
heuristic007 <hibaus@aol.com>
2018-01-04 05:56:49 -0800

    Create query_ex.php

---
+++ MySQL_ex/query_ex.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "sakila";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT actor_id, first_name, last_name FROM actor limit 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["actor_id"]. " - Name: ". $row["first_name"]. " " . $row["last_name"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
+
+</body>
+</html>
